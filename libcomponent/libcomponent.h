2b9485aa185fa4d36a0d9a9b74ba9e78e2c7e52d
Erik Ekstrom <erike90@gmail.com>
2015-09-27 12:40:15 +0200

    Added src files

---
+++ libcomponent/libcomponent.h
@@ -0,0 +1,6 @@
+/* Erik Ekström
+ * erek0016@student.umu.se
+ * 2015-09-26
+ *
+ * Uppgift 6: libcomponent.so
+ */
---
+++ libcomponent/libcomponent_test.c
@@ -0,0 +1,6 @@
+/* Erik Ekström
+ * erek0016@student.umu.se
+ * 2015-09-27
+ *
+ * Uppgift 6: libcomponent.so
+ */
